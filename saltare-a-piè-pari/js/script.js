c08d673a75848ff9a5e07b582c7d036b57b81d51
MatteoDiMario96 <dm.matteo96@live.it>
2024-05-13 16:47:52 +0200

    Aggiunta funzionalità che se è pari l'aggiunge nell'array altimenti non fa nulla.

--- saltare-a-piè-pari/js/script.js
+++ saltare-a-piè-pari/js/script.js
@@ -3,4 +3,8 @@ const listaPari = [];
 for(let i = 0; i < 6; i++){
     const numeroUtente = Number.parseInt(prompt("Dimmi un numero!"))
 
+    if(numeroUtente % 2 === 0){
+        listaPari.push(numeroUtente)
+    }
 }
+console.log(listaPari)
