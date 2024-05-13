e85d6ed071eb007394d9cb662d5fd5c5b9a4a7a4
MatteoDiMario96 <dm.matteo96@live.it>
2024-05-13 16:03:26 +0200

    Creati due variabili: numero giocatore e numero computer e assegnatogli un numero random da 1 a 6.

---
+++ js/script.js
@@ -0,0 +1,3 @@
+const numeroGiocatore = Number.parseInt(Math.floor(Math.random() * 6) + 1)
+const numeroComputer = Number.parseInt(Math.floor(Math.random() * 6) + 1)
+console.log(numeroGiocatore, numeroComputer)
