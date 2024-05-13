8e360d7eb93f112a0422cd0afae5853385a62c2f
MatteoDiMario96 <dm.matteo96@live.it>
2024-05-13 16:53:19 +0200

    Aggiunto il pareggio al gioco dei dadi.

--- gioco-dei-dadi/js/script.js
+++ gioco-dei-dadi/js/script.js
@@ -1,9 +1,11 @@
 const numeroGiocatore = Number.parseInt(Math.floor(Math.random() * 6) + 1)
 const numeroComputer = Number.parseInt(Math.floor(Math.random() * 6) + 1)
-console.log(numeroGiocatore, numeroComputer)
+console.log(numeroGiocatore, "VS", numeroComputer)
 
-if (numeroGiocatore > numeroComputer){
-    console.log("Congratulations, you have win !")
-} else {
+if (numeroGiocatore == numeroComputer){
+    console.log("Better the rain, same number !")
+} else if(numeroComputer > numeroGiocatore){
     console.log("I'm sorry. The pc bet you.")
+} else {
+    console.log("Congratulation,there is the sun today !")
 }
