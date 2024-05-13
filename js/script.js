461eae4aea6137b67284d1d6d50369114eb32816
MatteoDiMario96 <dm.matteo96@live.it>
2024-05-13 16:09:48 +0200

    modifica js ?

--- js/script.js
+++
@@ -1,3 +0,0 @@
-const numeroGiocatore = Number.parseInt(Math.floor(Math.random() * 6) + 1)
-const numeroComputer = Number.parseInt(Math.floor(Math.random() * 6) + 1)
-console.log(numeroGiocatore, numeroComputer)
