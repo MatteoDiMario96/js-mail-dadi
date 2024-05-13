788c7318eeec4db255a6fdd952b828e3cd34026e
MatteoDiMario96 <dm.matteo96@live.it>
2024-05-13 16:32:30 +0200

    Creato array, creato prompt per chiedere email all'utente. Creato modo che se l'utente digita caratteri maiusc, trasfomo tutto in minusc, visto che nell'email non ve ne sono.

---
+++ mail/js/script.js
@@ -0,0 +1,24 @@
+const listaEmail = [
+    "pincopallino@gmail.com",
+    "mariaantonietta@gmail.com",
+    "gisellaantoni@gmail.com",
+    "carlopalmieri@gmail.com",
+    "francoanselmi@gmail.com",
+    "ginoginetti@gmail.com",
+    "dinodinetti@gmail.com",
+    "simonesimonetti@gmail.com",
+    "paolopaoletti@gmail.com",
+    "marcomarchetti@gmail.com",
+    "zorrozorretti@gmail.com",
+    "adelmosuozzo@gmail.com"
+]
+
+let utenteEmail = prompt("Dicci la tua E-mail, Perfavore!")
+utenteEmail = utenteEmail.substring(0, utenteEmail.length).toLowerCase();
+// console.log(utenteEmail)
+
+// if(listaEmail.includes(utenteEmail)){
+//     console.log("Accesso: CONSENTITO")
+// } else{
+//     console.log("Accesso: NEGATO")
+// }
